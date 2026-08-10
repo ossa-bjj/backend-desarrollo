0d4b2bcaac0c3904dcf9c908d8e660e6b9bcbe13
kabalera82 <kabalera@gmail.com>
2026-08-10 11:05:47 +0200

    chore: dejar de versionar artefactos generados

--- dist/src/shared/token.utils.js
+++
@@ -1,3 +0,0 @@
-import jwt from 'jsonwebtoken';
-export const generateToken = (payload) => jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '8h' });
-export const verifyToken = (token) => jwt.verify(token, process.env.JWT_SECRET);
